9071ca5fea7805bbf2b038a2c1f8149d13d14347
smulrow <smulrow@gmail.com>
2022-08-05 15:14:26 -0700

    updating generateHTMl

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const fs = require('fs')
 const inquirer = require('inquirer')
-const generateMarkdown = require('./lib/generateMarkdown')
+const generateMarkdown = require('./lib/generateHTML')
 const Employee = require("./lib/employee");
-const {Engineer, engineerQuestions} = require("./lib/engineer")
-const {Manager, managerQuestions} = require("./lib/manager")
-const {Intern, internQuestions} = require("./lib/intern");
+const { Intern, internQuestions } = require('./lib/intern')
+const { Engineer, engineerQuestions} = require('./lib/engineer')
+const { Manager, managerQuestions} = require("./lib/manager")
 
-
+const employeeArr = []
 
 
 
@@ -14,6 +14,7 @@ function init() {
   inquirer.prompt(managerQuestions)
   .then((answers) => {
     answers = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
+    employeeArr.push(answers)
     return newEmployee()
   })
 }
@@ -44,62 +45,29 @@ function engineerQuestions2() {
   inquirer.prompt(engineerQuestions)
   .then(( answers ) => {
       answers = new Engineer(answers.name, answers.id, answers.email, answers.github)
-   
+      employeeArr.push(answers)
       return newEmployee();
   })
 }
 
-function internArr() {
-  inquirer.prompt(internQuestions)
-  .then((answers) => {
-    answers = new Intern(answers.name, answers.id, answers.email, answers.school)
 
-    return newEmployee()
+
+const internArr = () => {
+  inquirer.prompt(internQuestions)
+  .then(( answers ) => {
+      answers = new Intern(answers.name, answers.id, answers.email, answers.school)
+      employeeArr.push(answers);
+      return newEmployee();
   })
 }
 
 
-function buildTeam(){
-fs.writeFile("./dist/index.html", generateMarkdown(answers), Employee , (err) => 
-(err) ? console.error(err) : console.log("Success"))
+function buildTeam(employeeArr){
+  fs.writeFile("./dist/index.html", generateMarkdown(employeeArr) , (err) => 
+  (err) ? console.error(err) : console.log("Success"))
 }
+
 
 init();
 
 
-// const questions = [
-//   {
-//     type: 'input',
-//     message: 'What is the team managers name?',
-//     name: 'names'
-//   },
-//   {
-//     type: 'input',
-//     message: 'What is the team managers id?',
-//     name: 'position'
-//   },
-//   {
-//     type: 'input',
-//     message: 'What is the team managers email?',
-//     name: 'email'
-//   },
-//   {
-//     type: 'input',
-//     message: 'What is the team managers office nummber?',
-//     name: 'username'
-//   },
-//   {
-//     type: 'list',
-//     message: 'Which type of team member would you like to add?',
-//     choices: ['Engineer', 'Intern'],
-//     name: 'newTeamMember'
-//   }
-// ]
-
-// function init() {
-//   inquirer.prompt(questions)
-//   .then((answers) => {
-// })
-// };
-
-// init();
